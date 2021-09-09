064f216e9cacc3f4498102d0ad64bf6a6cd96328
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-09 16:33:10 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='hodgepodge',
+    version='0.1.1',
+    author='Tyler Fisher',
+    author_email='tylerfisher@tylerfisher.ca',
+    description="A hodgepodge of helpful code that's hopefully helpful to you.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+    ],
+    packages=find_packages(),
+    install_requires=[],
+)
