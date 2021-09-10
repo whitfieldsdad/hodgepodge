050f41ece03149de7b9f97e08c202dfe3e1d1e33
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-10 09:53:06 -0400

    Update poetry.lock, pyproject.toml, requirements.txt -- rename hodgepodge.requests to hodgepodge.web

--- hodgepodge/web.py
+++ hodgepodge/web.py
@@ -5,10 +5,15 @@ import requests
 import shutil
 import hodgepodge.files
 import hodgepodge.hashing
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def download_file(url: str, path: str, session: Optional[requests.Session] = None,
                   include_hashes: bool = INCLUDE_HASHES_BY_DEFAULT) -> Optional[Dict[str, str]]:
+
+    logging.info("Downloading {} -> {}".format(url, path))
 
     hodgepodge.files.mkdir(path)
     with open(path, 'wb') as fp:
