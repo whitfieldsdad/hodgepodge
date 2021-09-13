57c2246b96456b0cca93097187f7e1f5c24d16cd
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-13 08:42:32 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='hodgepodge',
-    version='0.1.6',
+    version='0.1.7',
     author='Tyler Fisher',
     author_email='tylerfisher@tylerfisher.ca',
     description="A hodgepodge of helpful code that's hopefully helpful to you.",
