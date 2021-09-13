02ff104a9bff02a0b7a3720f5f7291b48f30a3a1
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-13 01:11:42 -0400

    Add helper for translating string into a list of integers

--- hodgepodge/click.py
+++ hodgepodge/click.py
@@ -1,7 +1,15 @@
-from typing import List, Optional
+from typing import List
 
 
-def str_to_list(data: Optional[str]) -> List[str]:
-    if data is None:
+def str_to_list(data: str) -> List[str]:
+    return str_to_list_of_str(data)
+
+
+def str_to_list_of_str(data: str) -> List[str]:
+    if not data:
         return []
     return data.split(',')
+
+
+def str_to_list_of_int(data: str) -> List[int]:
+    return [int(v) for v in str_to_list_of_str(data)]
