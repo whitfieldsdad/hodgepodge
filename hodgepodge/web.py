976bc297ab8208ba4a426083ede0eb4cf15c01f9
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-13 01:13:46 -0400

    Add hodgepodge.web module

--- hodgepodge/web.py
+++ hodgepodge/web.py
@@ -1,26 +1,20 @@
+from requests import Session
 from typing import Optional, Dict
-from hodgepodge.constants import INCLUDE_HASHES_BY_DEFAULT
+from hodgepodge.constants import INCLUDE_HASHES_BY_DEFAULT, VERBOSE_BY_DEFAULT
 
-import requests
-import shutil
-import hodgepodge.files
-import hodgepodge.hashing
+import hodgepodge.requests
 import logging
 
 logger = logging.getLogger(__name__)
 
 
-def download_file(url: str, path: str, session: Optional[requests.Session] = None,
-                  include_hashes: bool = INCLUDE_HASHES_BY_DEFAULT) -> Optional[Dict[str, str]]:
+def download_file(url: str, path: str, session: Optional[Session] = None,
+                  include_hashes: bool = INCLUDE_HASHES_BY_DEFAULT, verbose: bool = VERBOSE_BY_DEFAULT) -> Optional[Dict[str, str]]:
 
-    logging.info("Downloading {} -> {}".format(url, path))
-
-    hodgepodge.files.mkdir(path)
-    with open(path, 'wb') as fp:
-        session = session or requests.Session()
-        response = session.get(url)
-        response.raise_for_status()
-
-        shutil.copyfileobj(response.raw, fp)
-        if include_hashes:
-            return hodgepodge.hashing.get_file_hashes(path)
+    return hodgepodge.requests.download_file(
+        url=url,
+        path=path,
+        session=session,
+        include_hashes=include_hashes,
+        verbose=verbose,
+    )
