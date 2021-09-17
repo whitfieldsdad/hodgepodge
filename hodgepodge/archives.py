727cb4d227fe553c7f1aca92e6b20149dc8ba527
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-17 12:34:12 -0400

    Add documentation for helper functions for working with ZIP archives

--- hodgepodge/archives.py
+++ hodgepodge/archives.py
@@ -1,5 +1,4 @@
 from typing import List
-from hodgepodge.constants import VERBOSE_BY_DEFAULT
 
 import hodgepodge.files
 import zipfile
@@ -9,15 +8,24 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def extract_archive(input_path: str, output_path: str, verbose: bool = VERBOSE_BY_DEFAULT):
-    if verbose:
-        logger.info("Extracting archive: %s -> %s")
+def extract_archive(input_path: str, output_path: str):
+    """
+    Extract the specified archive to the provided output path.
 
+    :param input_path: the path to the archive.
+    :param output_path: the path to extract the archive to.
+    """
     hodgepodge.files.mkdir(output_path)
     with zipfile.ZipFile(input_path, 'r') as fp:
         fp.extractall(output_path)
 
 
 def get_file_list(input_path: str) -> List[str]:
+    """
+    Return the path of each of the files within the specified archive.
+
+    :param input_path: the path to the archive.
+    :return: a list of filenames.
+    """
     with zipfile.ZipFile(input_path, 'r') as fp:
         return fp.namelist()
