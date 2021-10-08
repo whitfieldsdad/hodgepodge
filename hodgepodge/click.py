bcce405eb179f088a4a6dac94826021b25f9a616
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-10-08 11:38:43 -0400

    Remove hodgepodge.click.echo_as_json() function

--- hodgepodge/click.py
+++ hodgepodge/click.py
@@ -14,14 +14,3 @@ def str_to_strs(data: str) -> List[str]:
 
 def str_to_ints(data: str) -> List[int]:
     return [int(v) for v in str_to_strs(data)]
-
-
-def echo_as_json(data: Union[dict, Iterable[dict]]) -> None:
-    if isinstance(data, dict):
-        txt = json.dumps(data)
-        click.echo(txt)
-    elif hodgepodge.types.is_iterator(data):
-        for row in data:
-            echo_as_json(row)
-    else:
-        raise TypeError(type(data))
