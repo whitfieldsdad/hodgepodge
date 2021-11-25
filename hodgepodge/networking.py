16c2d62cfbf52db5ab4d51313efe03e149635a37
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-11-25 10:10:17 -0500

    Update format of function for parsing MAC addresses

--- hodgepodge/networking.py
+++ hodgepodge/networking.py
@@ -1,6 +1,9 @@
 import string
+from typing import Optional
 
 
-def parse_mac_address(value: str) -> str:
-    mac = ''.join(c for c in value if c in string.hexdigits)
-    return ':'.join(mac[i:i + 2] for i in range(0, 12, 2)).lower()
+def parse_mac_address(value: str) -> Optional[str]:
+    if value:
+        mac = ''.join(c for c in value if c in string.hexdigits)
+        mac = ':'.join(mac[i:i + 2] for i in range(0, 12, 2)).lower()
+        return mac
